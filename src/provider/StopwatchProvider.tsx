3991138c246e4f10f7c245dbe633b5d5b30a350f
tisavara.somboon <osacn_tisavara.somboon@krungthai.com>
2023-07-24 14:52:44 +0700

    feat: addd local storage

--- src/provider/StopwatchProvider.tsx
+++ src/provider/StopwatchProvider.tsx
@@ -4,6 +4,10 @@ interface StopwatchProviderProps {
   children: ReactNode
 }
 
+interface CurrentTime {
+  currentTime: number | null
+}
+
 interface LapTime {
   lap: number
   time: number
@@ -34,10 +38,18 @@ const StopwatchProviderContext = createContext<StopwatchProviderValue>({
 export const useStopwatch = () => useContext(StopwatchProviderContext)
 
 const StopwatchProvider = ({ children }: StopwatchProviderProps) => {
-  const defaultTime = { lapTime: [], isRunning: false }
+  const localLapTime: string | null = localStorage.getItem('lap')
+  const initialLapTime: CurrentTime & TimeInterval = localLapTime ? JSON.parse(localLapTime) : null
 
-  const [time, setTime] = useState(0)
+  const defaultTime = {
+    lapTime: initialLapTime?.lapTime ?? [],
+    isRunning: initialLapTime?.isRunning ?? false,
+  }
+
   const [{ isRunning, lapTime }, setTimeInterval] = useState<TimeInterval>(defaultTime)
+  const [time, setTime] = useState(
+    initialLapTime?.currentTime ?? defaultTime.lapTime[defaultTime.lapTime.length - 1]?.time ?? 0,
+  )
 
   const isResettable = time !== 0 && !isRunning
 
@@ -56,13 +68,18 @@ const StopwatchProvider = ({ children }: StopwatchProviderProps) => {
   }, [isRunning, time])
 
   const handleStartAndStop = () => {
-    setTimeInterval((prevState) => ({ ...prevState, isRunning: !prevState.isRunning }))
+    setTimeInterval((prevState) => {
+      const result = { ...prevState, isRunning: !prevState.isRunning, currentTime: time }
+
+      localStorage.setItem('lap', JSON.stringify(result))
+      return result
+    })
   }
 
   const handleLapAndReset = () => {
     setTimeInterval((prevState) => {
       if (!isResettable) {
-        return {
+        const timeInterval = {
           ...prevState,
           lapTime: [
             ...prevState.lapTime,
@@ -75,8 +92,12 @@ const StopwatchProvider = ({ children }: StopwatchProviderProps) => {
             },
           ],
         }
+
+        localStorage.setItem('lap', JSON.stringify({ ...timeInterval, currentTime: null }))
+        return timeInterval
       }
 
+      localStorage.setItem('lap', '')
       return defaultTime
     })
   }
